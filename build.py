99eca4f902e6bebe92d51f1f4993afb220ed50ec
Luis Diaz Mas <piponazo@gmail.com>
2017-07-02 19:18:05 +0200

    Specify where to upload the packages and additional repositories

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="piponazo", channel="testing")
+    builder = ConanMultiPackager(username="piponazo", channel="testing",
+                                 upload="https://api.bintray.com/conan/piponazo/piponazo",
+                                 remotes="https://api.bintray.com/conan/conan/conan-center")
     builder.add_common_builds(shared_option_name="Expat:shared")
     builder.run()
