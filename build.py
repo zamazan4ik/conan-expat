fd799417d6a85569ecf999b23a3d14dc54b817b5
Luis Diaz Mas <piponazo@gmail.com>
2017-07-02 22:15:13 +0200

    Remove conan-center from repositories (It's already used)

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="piponazo", channel="testing",
-                                 upload="https://api.bintray.com/conan/piponazo/piponazo",
-                                 remotes="https://api.bintray.com/conan/conan/conan-center")
+                                 upload="https://api.bintray.com/conan/piponazo/piponazo")
     builder.add_common_builds(shared_option_name="Expat:shared")
     builder.run()
